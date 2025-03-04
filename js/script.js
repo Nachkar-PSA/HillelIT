e5d2095d94e77411e2f2444832aec21901375be8
Serhii Pokhodenko <nachkar.santyago@gmail.com>
2025-03-04 13:45:29 -0500

    remove unused VSCode settings and launch configurations; update close button functionality and add login button event listener

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Close the form
+// Close the form and back to index.ntml
+const closeButton = document.querySelector(".form__close");
+if (closeButton) {
+  closeButton.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+}
 
-document.querySelector(".form__close").addEventListener("click", function () {
-  document.querySelector(".reg__wrapper").style.display = "none";
-});
+// Add the Event Listener for Login Button and go to registration.html
+const loginButton = document.getElementById("loginButton");
+if (loginButton) {
+  loginButton.addEventListener("click", function () {
+    window.location.href = "registration.html";
+  });
+}
